8af46b37bcf927486d897b8c18a3109b68b3c581
eat-sleep-code/ <info@eat-sleep-code.com>
2019-05-26 08:19:52 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
-var ccmd = "";
-var current_que = 'main';
-var current_playlist_id = -1;
-var intv = 0;
-var pollStatus = true;
-var previous_playlist_id = -1;
-var stream_server = window.location.hostname;
+var clickCommand = "";
+var interval = 0;
+var currentPlaylistId = -1;
+var previousPlaylistId = -1;
 
 
 
-function format_time(s) {
+function formatTime(s) {
 	var hours = Math.floor(s / 3600);
 	var minutes = Math.floor((s / 60) % 60);
 	var seconds = Math.floor(s % 60);
@@ -26,23 +23,12 @@ function toFloat(text) {
 
 
 
-function setIntv() {
-	if (intv > 0) {
-		intv++;
-		setTimeout(setIntv, 500);
+function setInterval() {
+	if (interval > 0) {
+		interval++;
+		setTimeout(setInterval, 500);
 	} else {
-		intv = 0;
-	}
-	if (intv > 5) {
-		var nt = 0;
-		switch (ccmd) {
-			case 'prev':
-				nt = Math.max(0, $('#seekSlider').slider('value') - 10);
-				break;
-			case 'next':
-				nt = Math.max(0, $('#seekSlider').slider('value') + 10);
-				break;
-		}
+		interval = 0;
 	}
 }
 
@@ -52,38 +38,34 @@ function updateStatus() {
 	$.ajax({
 		url: 'requests/status.xml',
 		success: function (data, status, jqXHR) {
-			if (current_que == 'main') {
-				//console.log(data);
-				$('.dynamic').empty();
-				if ($('[name="title"]', data).text().length > 0) {
-					$('#sourceStream').append($('[name="title"]', data).text());
-				} else {
-					$('#sourceStream').append($('[name="filename"]', data).text());
-				}
-				$('#sourceNowPlaying').append($('[name="now_playing"]', data).text());
-				$('#sourcePlayTime').append(format_time($('time', data).text()));
-				$('#currentVolume').append(Math.round($('volume', data).text() / 2.56) + '%');
-				if (!$('#volumeSlider').data('clicked')) {
-					$('#volumeSlider').slider({
-						value: ($('volume', data).text() / 5.12)
-					});
-				}
-				$('#buttonPlay').attr('state', $('state', data).text()).attr('mrl', $('[name="filename"]', data).text());
-				if ($('state', data).text() == 'playing') {
-					$('#buttonPlay').removeClass('fa-play').addClass('fa-pause');
-				} else {
-					$('#buttonPlay').removeClass('fa-pause').addClass('fa-play');
-				}
-
-				current_playlist_id = parseInt($('currentplid', data).text());
-				if (previous_playlist_id != current_playlist_id) {
-					previous_playlist_id = current_playlist_id;
-				}
-
-				if (pollStatus) {
-					setTimeout(updateStatus, 1000);
-				}
+			//console.log(data);
+			$('.dynamic').empty();
+			if ($('[name="title"]', data).text().length > 0) {
+				$('#sourceStream').append($('[name="title"]', data).text());
+			} else {
+				$('#sourceStream').append($('[name="filename"]', data).text());
 			}
+			$('#sourceNowPlaying').append($('[name="now_playing"]', data).text());
+			$('#sourcePlayTime').append(formatTime($('time', data).text()));
+			$('#currentVolume').append(Math.round($('volume', data).text() / 2.56) + '%');
+			if (!$('#volumeSlider').data('clicked')) {
+				$('#volumeSlider').slider({
+					value: ($('volume', data).text() / 5.12)
+				});
+			}
+			$('#buttonPlay').attr('state', $('state', data).text()).attr('mrl', $('[name="filename"]', data).text());
+			if ($('state', data).text() == 'playing') {
+				$('#buttonPlay').removeClass('fa-play').addClass('fa-pause');
+			} else {
+				$('#buttonPlay').removeClass('fa-pause').addClass('fa-play');
+			}
+
+			currentPlaylistId = parseInt($('currentplid', data).text());
+			if (previousPlaylistId != currentPlaylistId) {
+				previousPlaylistId = currentPlaylistId;
+			}
+
+			setTimeout(updateStatus, 1000);
 		},
 		error: function (jqXHR, status, error) {
 			setTimeout(updateStatus, 500);
@@ -108,24 +90,24 @@ function sendCommand(params, append) {
 
 
 $(function () {
-	$("#volumeSlider").slider({
+	$('#volumeSlider').slider({
 		range: "min",
 		value: 50,
 		min: 0,
 		max: 100,
 		start: function (event, ui) {
-			$("#volumeSlider").data('clicked', true);
+			$('#volumeSlider').data('clicked', true);
 		},
 		stop: function (event, ui) {
-			$("#currentVolume").empty().append(ui.value + "%");
+			$('#currentVolume').empty().append(ui.value + "%");
 			sendCommand({
 				'command': 'volume',
 				'val': Math.round(ui.value * 5.12)
 			})
-			$("#volumeSlider").data('clicked', false);
+			$('#volumeSlider').data('clicked', false);
 		}
 	});
-	$("#volumeSlider").data('clicked', false);
+	$('#volumeSlider').data('clicked', false);
 
 	$('#buttonStop').click(function () {
 		sendCommand({
@@ -138,7 +120,7 @@ $(function () {
 		if ($(this).attr('state') == 'stopped') {
 			sendCommand({
 				'command': 'pl_play',
-				'id': current_playlist_id
+				'id': currentPlaylistId
 			});
 		} else {
 			sendCommand({
@@ -149,31 +131,42 @@ $(function () {
 	});
 
 	$('#buttonPrev').mousedown(function () {
-		intv = 1;
-		ccmd = 'prev';
-		setIntv();
+		interval = 1;
+		clickCommand = 'prev';
+		setInterval();
 		return false;
 	});
 	$('#buttonPrev').mouseup(function () {
-		if (intv <= 5) {
+		if (interval <= 5) {
 			sendCommand({ 'command': 'pl_previous' });
 		}
-		intv = 0;
+		interval = 0;
 		return false;
 	});
 
 	$('#buttonNext').mousedown(function () {
-		intv = 1;
-		ccmd = 'next';
-		setIntv();
+		interval = 1;
+		clickCommand = 'next';
+		setInterval();
 		return false;
 	});
 	$('#buttonNext').mouseup(function () {
-		if (intv <= 5) {
+		if (interval <= 5) {
 			sendCommand({ 'command': 'pl_next' });
 		}
-		intv = 0;
+		interval = 0;
 		return false;
+	});
+
+	$('#buttonAdd').click(function() {
+		var quickAddUrl = prompt('Enter the url of the stream that you wish to listen to:');
+		if (quickAddUrl) {
+			sendCommand({
+				'command': 'in_play',
+				'input': quickAddUrl,
+				'option': 'novideo'
+			})
+		}
 	});
 
 	updateStatus();
